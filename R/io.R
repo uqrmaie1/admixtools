38f6c658fb3de5cf4f25af98deb47e8921cb11dc
Oluwatosin Olayinka <demonslayerguy@gmail.com>
2026-04-17 15:09:41 -0400

    moved duplicate ID check till AFTER filtering

--- R/io.R
+++ R/io.R
@@ -2256,13 +2256,14 @@ f4blockdat_from_geno = function(pref, popcombs = NULL, left = NULL, right = NULL
     stop(paste0('Populations missing from indfile: ', paste0(setdiff(pops, indfile$pop), collapse = ', ')))
   if(!is.null(block_lengths) && sum(block_lengths) != nsnpaut)
     stop(paste0('block_lengths should sum to ', nsnpaut,' (the number of autosomal SNPs)'))
-  if(any(duplicated(indfile$ind)))
-    stop('Duplicate individual IDs are not allowed!')
-
   allinds = indfile$ind
   allpops = indfile$pop
   indfile %<>% filter(pop %in% pops)
-  indvec = (allinds %in% indfile$ind)+0
+  indvec = ((allinds %in% indfile$ind) & (allpops %in% indfile$pop))+0
+  if(any(duplicated(indfile$ind)))
+    stop('Duplicate individual IDs are not allowed!')
+
+
   popvec = match(indfile$pop, pops)
   p1 = match(pc$pop1, pops)
   p2 = match(pc$pop2, pops)
@@ -2387,13 +2388,15 @@ f3blockdat_from_geno = function(pref, popcombs, auto_only = TRUE,
     stop(paste0('Populations missing from indfile: ', paste0(setdiff(pops, indfile$pop), collapse = ', ')))
   if(!is.null(block_lengths) && sum(block_lengths) != nsnpaut)
     stop(paste0('block_lengths should sum to ', nsnpaut,' (the number of autosomal SNPs)'))
-  if(any(duplicated(indfile$ind)))
-    stop('Duplicate individual IDs are not allowed!')
 
   allinds = indfile$ind
   allpops = indfile$pop
   indfile %<>% filter(pop %in% pops)
-  indvec = (allinds %in% indfile$ind)+0
+  indvec = ((allinds %in% indfile$ind) & (allpops %in% indfile$pop))+0
+  if(any(duplicated(indfile$ind)))
+    stop('Duplicate individual IDs are not allowed!')
+
+
   popvec = match(indfile$pop, pops)
   p1 = match(pc$pop1, pops)
   p2 = match(pc$pop2, pops)
